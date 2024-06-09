a567bebc6546e87c37b8d0fbe279d018394fcf52
frost-1256 <habanero.iori@gmail.com>
2024-06-09 10:16:42 +0900

    プロジェクト ファイルを追加します。

---
+++ PicConverter/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PicConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            title = new Panel();
+            rightPanel = new Panel();
+            label3 = new Label();
+            leftPanel = new Panel();
+            label2 = new Label();
+            title.SuspendLayout();
+            rightPanel.SuspendLayout();
+            leftPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Bizin Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(218, 27);
+            label1.TabIndex = 0;
+            label1.Text = "iconかpngに変換";
+            // 
+            // title
+            // 
+            title.Controls.Add(label1);
+            title.Dock = DockStyle.Top;
+            title.Location = new Point(0, 0);
+            title.Name = "title";
+            title.Size = new Size(412, 36);
+            title.TabIndex = 1;
+            // 
+            // rightPanel
+            // 
+            rightPanel.BorderStyle = BorderStyle.FixedSingle;
+            rightPanel.Controls.Add(label3);
+            rightPanel.Dock = DockStyle.Right;
+            rightPanel.Location = new Point(212, 36);
+            rightPanel.Name = "rightPanel";
+            rightPanel.Size = new Size(200, 180);
+            rightPanel.TabIndex = 2;
+            rightPanel.DragDrop += rightPanel_DragDrop;
+            rightPanel.DragEnter += rightPanel_DragEnter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Bizin Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label3.Location = new Point(16, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 13);
+            label3.TabIndex = 1;
+            label3.Text = "icoに";
+            // 
+            // leftPanel
+            // 
+            leftPanel.BorderStyle = BorderStyle.FixedSingle;
+            leftPanel.Controls.Add(label2);
+            leftPanel.Dock = DockStyle.Left;
+            leftPanel.Location = new Point(0, 36);
+            leftPanel.Name = "leftPanel";
+            leftPanel.Size = new Size(198, 180);
+            leftPanel.TabIndex = 3;
+            leftPanel.DragDrop += leftPanel_DragDrop;
+            leftPanel.DragEnter += leftPanel_DragEnter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Bizin Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label2.Location = new Point(12, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 13);
+            label2.TabIndex = 0;
+            label2.Text = "pngに";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(6F, 11F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(412, 216);
+            Controls.Add(leftPanel);
+            Controls.Add(rightPanel);
+            Controls.Add(title);
+            MaximizeBox = false;
+            MaximumSize = new Size(420, 247);
+            MinimumSize = new Size(420, 247);
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "画像変換";
+            TopMost = true;
+            Load += Form1_Load;
+            title.ResumeLayout(false);
+            title.PerformLayout();
+            rightPanel.ResumeLayout(false);
+            rightPanel.PerformLayout();
+            leftPanel.ResumeLayout(false);
+            leftPanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel title;
+        private Panel rightPanel;
+        private Label label3;
+        private Panel leftPanel;
+        private Label label2;
+    }
+}
---
+++ PicConverter/Form1.cs
@@ -0,0 +1,108 @@
+using System.Drawing.Imaging;
+
+namespace PicConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rightPanel.AllowDrop = true;
+            leftPanel.AllowDrop = true;
+        }
+
+        private void leftPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void leftPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            ConvertDroppedFiles(e.Data.GetData(DataFormats.FileDrop), ImageFormat.Png, leftPanel);
+        }
+
+        private void rightPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void rightPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            ConvertDroppedFiles(e.Data.GetData(DataFormats.FileDrop), ImageFormat.Icon, rightPanel);
+        }
+
+        private void ConvertDroppedFiles(object data, ImageFormat format, Panel panel)
+        {
+            string[] files = (string[])data;
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    // 画像ファイル以外の場合
+                    if (!IsImageFile(file))
+                    {
+                        MessageBox.Show("画像ファイルを入力してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        continue;
+                    }
+
+                    using (Image image = Image.FromFile(file))
+                    {
+                        string outputDirectory = Path.GetDirectoryName(file);
+                        string outputFileName = $"{Path.GetFileNameWithoutExtension(file)}.{format.ToString().ToLower()}";
+                        string outputFilePath = Path.Combine(outputDirectory, outputFileName);
+
+                        if (File.Exists(outputFilePath))
+                        {
+                            DialogResult result = MessageBox.Show($"ファイル '{outputFileName}' は既に存在します。上書きしますか?", "確認", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                            if (result == DialogResult.Cancel)
+                            {
+                                continue; // 次のファイルに進む
+                            }
+                            else if (result == DialogResult.No)
+                            {
+                                // 別名で保存する
+                                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                                saveFileDialog.FileName = outputFileName;
+                                saveFileDialog.InitialDirectory = outputDirectory;
+                                saveFileDialog.Filter = $"{format.ToString()} ファイル (*{format.ToString().ToLower()})|*{format.ToString().ToLower()}";
+
+                                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                                {
+                                    outputFilePath = saveFileDialog.FileName;
+                                }
+                                else
+                                {
+                                    continue; // キャンセルされた場合は次のファイルに進む
+                                }
+                            }
+                        }
+
+                        image.Save(outputFilePath, format);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private bool IsImageFile(string filePath)
+        {
+            string extension = Path.GetExtension(filePath).ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".bmp" || extension == ".gif" || extension == ".tiff";
+        }
+    }
+}
